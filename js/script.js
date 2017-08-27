250c30b32ed3970f20828121a278eb2cfa4467ec
SaraKelly89 <31234346+SaraKelly89@users.noreply.github.com>
2017-08-27 11:38:09 +1000

    27/08/17

--- js/script.js
+++ js/script.js
@@ -7,23 +7,23 @@ $( document ).ready(function() {
   });
 
 $(".owl-carousel").owlCarousel();
-loop:true,
-    margin:10,
-    nav:true,
-responsiveClass:True,
-responsive:{
-    // breakpoint from 0 up
-    0 : {
-        items:1
-    },
-    // breakpoint from 480 up
-    469 : {
-        items:2
-    },
-    // breakpoint from 768 up
-    1000 : {
-      items:3
-    }
-}
+  loop:true,
+  margin:10,
+  nav:true,
+  responsiveClass:True,
+  responsive:{
+      // breakpoint from 0 up
+      0 : {
+          items:1
+      },
+      // breakpoint from 480 up
+      469 : {
+          items:2
+      },
+      // breakpoint from 768 up
+      1000 : {
+        items:3
+      }
+  }
 
 });
